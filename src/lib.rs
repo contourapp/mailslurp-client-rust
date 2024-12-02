aba85bb397ca6cd9ca0730ff32b62cf24c0ded61
Alex Propp <alexpropp@gmail.com>
2024-12-02 20:00:29 -0300

    Updating reqwest and removing lint warnings.

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,19 @@
+#![allow(
+    unused_mut,
+    unused_variables,
+    clippy::empty_docs,
+    clippy::into_iter_on_ref,
+    clippy::new_without_default,
+    clippy::too_many_arguments
+)]
+
 #[macro_use]
 extern crate serde_derive;
 
+extern crate reqwest;
 extern crate serde;
 extern crate serde_json;
 extern crate url;
-extern crate reqwest;
 
 pub mod apis;
 pub mod models;
